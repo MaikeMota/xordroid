fa9c14092bef64f3bc62e33af88e29919c5a2c11
Maike Mota <maike.mota@atos.net>
2021-02-01 22:39:30 -0300

    feat: added util for send whispers
     - Improvement:
       - add hability to sent multiple messages

--- utils.js
+++ utils.js
@@ -1,11 +1,23 @@
 module.exports = {
     isCommand(possibleCommand) {
-        return possibleCommand.startsWith(process.env.COMMAND_PREFIX);
+        return possibleCommand.startsWith(process.env.COMMAND_PREFIX || `!`);
     },
-    async sendMessage(channel, client, message) {
-        await client.say(
-            channel,
-            message
-        );
+    async sendMessage(channel, client, ...messages) {
+        if (messages) {
+            messages.forEach(async message => {
+                await client.say(
+                    channel,
+                    message
+                );
+            })
+        }
+    },
+    async sendWhisperMessage(client, username, ...messages) { 
+        if(messages) { 
+            messages.forEach(async message => { 
+                await client.whisper(username, message);
+            })
+        }
+
     }
 }
