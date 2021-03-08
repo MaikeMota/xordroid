89121e2eef7ff9dc1b05c4ed23f10de20a405734
Maike Mota <maike.mota@atos.net>
2021-03-08 20:48:20 -0300

    migrated to new model

--- commands/tts.js
+++ commands/tts.js
@@ -1,22 +1,77 @@
-exports.default = (client, obs, mqtt, messages, botDB, commandQueue, ttsQueue) => {
-    client.on('message', (target, context, message, isBot) => {
-        if (isBot) return;
+const sound = require("sound-play");
 
-        let parsedMessage = message.split(" ");
-        if(parsedMessage[0] === '!tts') {
-          let fullMessage = context["display-name"] + " disse: " + message.replace("!tts ","");
-          ttsQueue.push( {'msg': fullMessage, 'lang': 'pt-BR','inputType': 'text'});
-        }
+const util = require('util');
+const fs = require('fs');
+const path = require('path');
 
-        if(parsedMessage[0] === '!entts') {
-          let fullMessage = context["display-name"] + " says: " + message.replace("!entts ","");
-          ttsQueue.push( {'msg': fullMessage, 'lang': 'en','inputType': 'text'});
-        }
+const textToSpeech = require('@google-cloud/text-to-speech');
+const clientTTS = new textToSpeech.TextToSpeechClient();
 
-        if(parsedMessage[0] === '!pttts') {
-          let fullMessage = context["display-name"] + " disse: " + message.replace("!pttts ","");
-          ttsQueue.push( {'msg': fullMessage, 'lang': 'pt-PT','inputType': 'text'});
-        }
-    });
+const queueTTS = (username, message, lang) => {
+  let fullMessage = `${username} ${lang == 'en' ? 'said' : 'disse'}: ${message}`;
+  ttsQueue.push({ 'msg': fullMessage, 'lang': lang, 'inputType': 'text' });
 };
 
+exports.default = [
+  {
+    command: 'tts',
+    handler: (client, channel, requestor, args) => {
+      queueTTS(requestor, args.join(' '), 'pt-BR');
+    },
+  },
+  {
+    command: 'entts',
+    handler: (client, channel, requestor, args) => {
+      if (!this.client) { // TODO get client instance in a better way
+        this.client = client;
+      }
+      queueTTS(requestor, args.join(' '), 'en');
+    },
+  },
+  {
+    command: 'pttts',
+    handler: (client, channel, requestor, args) => {
+      if (!this.client) { // TODO get client instance in a better way
+        this.client = client;
+      }
+      queueTTS(requestor, args.join(' '), 'pt-PT');
+    },
+  }
+]
+
+const ttsQueue = [];
+let isPlayingTTS = false;
+
+const ttsInterval = setInterval(async () => {
+  if (!isPlayingTTS && ttsQueue.length > 0) {
+    let tts = ttsQueue.shift();
+    await playTTS(tts);
+  }
+}, 500);
+
+async function playTTS(message) {
+  isPlayingTTS = true;
+  const text = message.msg;
+  const inputType = message.inputType;
+
+  const request = {
+    input: { [inputType]: text },
+    voice: { languageCode: message.lang, ssmlGender: 'NEUTRAL' },
+    audioConfig: { audioEncoding: 'LINEAR16' },
+  };
+
+  const [response] = await clientTTS.synthesizeSpeech(request);
+  const writeFile = util.promisify(fs.writeFile);
+
+  try {
+    const ttsTempFile = path.resolve('output.m4a');
+    await writeFile(ttsTempFile, response.audioContent);
+    await sound.play(ttsTempFile);
+    console.log("done");
+  } catch (error) {
+    console.error('Error whiling reproducing TTS sound file', error);
+  } finally {
+    isPlayingTTS = false;
+  }
+
+};
