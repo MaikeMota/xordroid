6303fb1a213072078594d1bef32ca4f58dc813a5
Maike Mota <maike.mota@atos.net>
2021-03-08 22:41:05 -0300

    make streamer name be case insensitive

--- commands/sh-so.js
+++ commands/sh-so.js
@@ -5,8 +5,8 @@ const { SH_SO_SUPPORTED_STREAMERS } = process.env;
 
 const streamers = (SH_SO_SUPPORTED_STREAMERS || "")
   .split(',')
-  .reduce((prev, current, index) => {
-    prev[current] = false;
+  .reduce((prev, current) => {
+    prev[current.toLocaleLowerCase()] = false;
     return prev;
   }, {});
 
@@ -14,14 +14,16 @@ exports.default = {
   event: TWITCH_EVENTS.JOIN,
   command: 'sh-so-handler',
   handler: async (client, channel, username) => {
+    username = username.toLowerCase();
     const supportedStreamer = !!streamers[username];
     if (supportedStreamer) {
       if (!streamers[username])
+        console.log(`[sh-so-handler] Supported Streamer ${username} joined the streaming chat.`);
         sendMessage(channel, client, `Opa! ${username} na área, já conhece o canal?`);
         sendMessage(channel, client, `!sh-so @${username}`);
         streamers[username] = true;
     } else { 
-      console.log(`${username} joined the streaming chat.`);
+      console.log(`[sh-so-handler] ${username} joined the streaming chat.`);
     }
   }
 }
